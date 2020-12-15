36260f1ea0b94877c297171507a46662d0db2708
huisheng.jin <huisheng.jinge@gmail.com>
2020-12-15 22:02:00 +0800

    empty message

--- src/main/java/Anagrams.java
+++ src/main/java/Anagrams.java
@@ -14,8 +14,10 @@ public class Anagrams {
         }
         List<String> result = new ArrayList<>();
         for (int index = 0; index < word.length(); index++) {
-            char indexChar = word.charAt(index);
-            of(deleteChar(word, index)).forEach(str -> result.add(indexChar + str));
+            List<String> anagrameOfLeft = of(deleteChar(word, index));
+            for (String str : anagrameOfLeft) {
+                result.add(word.charAt(index) + str);
+            }
         }
         return result;
     }
