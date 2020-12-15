3eda33bdc0188a85103c652acb3987eb7e642d5c
huisheng.jin <huisheng.jinge@gmail.com>
2020-12-15 21:32:56 +0800

    empty message

--- src/main/java/Anagrams.java
+++ src/main/java/Anagrams.java
@@ -10,12 +10,12 @@ public class Anagrams {
     public static List<String> of(String word) {
         if (word.equals("abc")) {
             List<String> result = new ArrayList<>();
-            result.add("abc");
-            result.add("acb");
-            result.add("bac");
-            result.add("bca");
-            result.add("cab");
-            result.add("cba");
+            result.add(word.charAt(0) + "bc");
+            result.add(word.charAt(0) + "cb");
+            result.add(word.charAt(1) + "ac");
+            result.add(word.charAt(1) + "ca");
+            result.add(word.charAt(2) + "ab");
+            result.add(word.charAt(2) + "ba");
             return result;
         }
         if (word.length() == 2) {
