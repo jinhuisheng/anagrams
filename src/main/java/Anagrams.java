3ff472e2d1ee0bb5006fb9cd7ccb46652b21c88e
huisheng.jin <huisheng.jinge@gmail.com>
2020-12-15 19:59:24 +0800

    empty message

---
+++ src/main/java/Anagrams.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+/**
+ * @author huisheng.jin
+ * @date 2020/12/15.
+ */
+public class Anagrams {
+    public static List<String> of(String a) {
+        return null;
+    }
+}
---
+++ src/test/java/AnagramsTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+/**
+ * @author huisheng.jin
+ * @date 2020/12/15.
+ */
+public class AnagramsTest {
+    @Test
+    void should_one_word_success() {
+        List<String> result = Anagrams.of("a");
+    }
+}
