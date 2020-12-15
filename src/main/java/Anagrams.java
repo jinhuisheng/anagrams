4da218e1cf549539fca074c142101a43d52552b3
huisheng.jin <huisheng.jinge@gmail.com>
2020-12-15 20:18:26 +0800

    empty message

--- src/main/java/Anagrams.java
+++ src/main/java/Anagrams.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,18 +9,37 @@ import java.util.stream.Collectors;
  * @date 2020/12/15.
  */
 public class Anagrams {
+    private String word;
+    private List<String> result;
+
+    public Anagrams(String word) {
+        this.result = new ArrayList<>();
+        this.word = word;
+    }
+
     public static List<String> of(String word) {
+        Anagrams anagrams = new Anagrams(word);
+        return anagrams.of();
+    }
+
+    private List<String> of() {
         if (word.length() == 1) {
-            return Arrays.asList(word);
+            return Collections.singletonList(word);
         }
-        List<String> result = new ArrayList<>();
-        for (int i = 0; i < word.length(); i++) {
-            char currentChar = word.charAt(i);
-            String deletedCharWord = new StringBuilder(word).deleteCharAt(i).toString();
-            List<String> collect = of(deletedCharWord).stream().map(str -> currentChar + str).collect(Collectors.toList());
-            result.addAll(collect);
+        for (int index = 0; index < word.length(); index++) {
+            add(index);
         }
         return result;
+    }
+
+    private void add(int i) {
+        char currentChar = word.charAt(i);
+        String deletedCharWord = new StringBuilder(word).deleteCharAt(i).toString();
+        List<String> deletedCharWordResult = of(deletedCharWord);
+        List<String> collect = deletedCharWordResult.stream()
+                .map(str -> currentChar + str)
+                .collect(Collectors.toList());
+        result.addAll(collect);
     }
 
 }
