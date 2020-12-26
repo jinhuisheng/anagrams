7b56ba8d8f68378be9cd91c69197b22dc84f20c6
huisheng.jin <huisheng.jinge@gmail.com>
2020-12-26 21:42:25 +0800

    empty message

--- src/main/java/Anagrams.java
+++ src/main/java/Anagrams.java
@@ -1,10 +1,14 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Anagrams {
     public static List<String> of(String word) {
+        List<String> list = new ArrayList<>();
         if (word.equals("ab")) {
-            return Arrays.asList("ab", "ba");
+            list.add("ab");
+            list.add("ba");
+            return list;
         }
         return Arrays.asList("a");
     }
