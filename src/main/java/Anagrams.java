a98142658b9ac23c3b1680b595b63fd5d83bb6c4
huisheng.jin <huisheng.jinge@gmail.com>
2020-12-26 21:55:55 +0800

    empty message

--- src/main/java/Anagrams.java
+++ src/main/java/Anagrams.java
@@ -4,23 +4,27 @@ import java.util.List;
 
 public class Anagrams {
     public static List<String> of(String word) {
-        if (word.equals("abc")) {
-            List<String> list = new ArrayList<>();
-            String[] wordArray = word.split("");
-            for (int index = 0; index < 3; index++) {
-                for (String one : of(delChar(word, index))) {
-                    list.add(wordArray[index] + one);
-                }
-            }
-            return list;
-        }
         if (word.length() == 2) {
-            List<String> list = new ArrayList<>();
-            list.add(word);
-            list.add(reverse(word));
-            return list;
+            return convertWordLenthTwo(word);
         }
-        return Arrays.asList(word);
+        if (word.length() == 1) {
+            return Arrays.asList(word);
+        }
+        List<String> list = new ArrayList<>();
+        String[] wordArray = word.split("");
+        for (int index = 0; index < 3; index++) {
+            for (String one : of(delChar(word, index))) {
+                list.add(wordArray[index] + one);
+            }
+        }
+        return list;
+    }
+
+    private static List<String> convertWordLenthTwo(String word) {
+        List<String> list = new ArrayList<>();
+        list.add(word);
+        list.add(reverse(word));
+        return list;
     }
 
     private static String delChar(String word, int index) {
