30aec59b29e7224c984fc6a06d0dfdb995506c57
huisheng.jin <huisheng.jinge@gmail.com>
2020-12-28 08:43:18 +0800

    empty message

--- src/main/java/Anagrams.java
+++ src/main/java/Anagrams.java
@@ -6,19 +6,19 @@ import static org.apache.commons.lang3.StringUtils.reverse;
 
 public class Anagrams {
     public static List<String> of(String word) {
-        if (word.length() == 3) {
-            List<String> list = new ArrayList<>();
-            for (int i = 0; i < word.length(); i++) {
-                for (String str : of(delChar(word, i))) {
-                    list.add(word.charAt(i) + str);
-                }
-            }
-            return list;
-        }
         if (word.length() == 2) {
             return Arrays.asList(word, reverse(word));
         }
-        return Arrays.asList(word);
+        if (word.length() == 1) {
+            return Arrays.asList(word);
+        }
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < word.length(); i++) {
+            for (String str : of(delChar(word, i))) {
+                list.add(word.charAt(i) + str);
+            }
+        }
+        return list;
     }
 
     private static String delChar(String word, int index) {
