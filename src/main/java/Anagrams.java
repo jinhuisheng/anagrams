f0d8421c4a69b5ec0aa219cec4346febfd210e7b
huisheng.jin <huisheng.jinge@gmail.com>
2020-12-28 09:49:20 +0800

    empty message

--- src/main/java/Anagrams.java
+++ src/main/java/Anagrams.java
@@ -7,8 +7,8 @@ public class Anagrams {
     public static List<String> of(String word) {
         if (word.length() == 3) {
             return Arrays.asList(
-                    word.charAt(0) + delChar(word, 0),
-                    word.charAt(0) + "cb",
+                    word.charAt(0) + of(delChar(word, 0)).get(0),
+                    word.charAt(0) + of(delChar(word, 0)).get(1),
                     "bac",
                     "bca",
                     "cab",
