82f4aebc12744ddc698858e0ee8743da25e0a3ee
huisheng.jin <huisheng.jinge@gmail.com>
2020-12-28 09:50:25 +0800

    empty message

--- src/main/java/Anagrams.java
+++ src/main/java/Anagrams.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -6,14 +7,21 @@ import static org.apache.commons.lang3.StringUtils.reverse;
 public class Anagrams {
     public static List<String> of(String word) {
         if (word.length() == 3) {
-            return Arrays.asList(
-                    word.charAt(0) + of(delChar(word, 0)).get(0),
-                    word.charAt(0) + of(delChar(word, 0)).get(1),
-                    "bac",
-                    "bca",
-                    "cab",
-                    "cba"
-            );
+            List<String> list = new ArrayList<>();
+            for (int i = 0; i < 3; i++) {
+                list.add(word.charAt(i) + of(delChar(word, i)).get(0));
+                list.add(word.charAt(i) + of(delChar(word, i)).get(1));
+            }
+            return list;
+//
+//            return Arrays.asList(
+//                    word.charAt(0) + of(delChar(word, 0)).get(0),
+//                    word.charAt(0) + of(delChar(word, 0)).get(1),
+//                    "bac",
+//                    "bca",
+//                    "cab",
+//                    "cba"
+//            );
         }
         if (word.length() == 2) {
             return Arrays.asList(word, reverse(word));
