e407e936b24a046fe4b6f95325e56618876f76fc
huisheng.jin <huisheng.jinge@gmail.com>
2020-12-28 08:33:32 +0800

    empty message

--- src/test/java/AnagramsTest.java
+++ src/test/java/AnagramsTest.java
@@ -9,4 +9,10 @@ public class AnagramsTest {
     void should_generate_one_char_word() {
         assertThat(Anagrams.of("a")).isEqualTo(Arrays.asList("a"));
     }
+
+    @Test
+    void should_generate_two_char_word() {
+        assertThat(Anagrams.of("ab")).isEqualTo(Arrays.asList("ab", "ba"));
+    }
+
 }
