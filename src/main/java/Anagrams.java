eb6a934adc1bcac8e3cf56dba49a2b091a7acb6c
huisheng.jin <huisheng.jinge@gmail.com>
2020-12-28 08:32:41 +0800

    empty message

--- src/main/java/Anagrams.java
+++ src/main/java/Anagrams.java
@@ -1,37 +1,8 @@
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Anagrams {
     public static List<String> of(String word) {
-        if (word.length() == 2) {
-            return convertWordLenthTwo(word);
-        }
-        if (word.length() == 1) {
-            return Arrays.asList(word);
-        }
-        List<String> list = new ArrayList<>();
-        String[] wordArray = word.split("");
-        for (int index = 0; index < word.length(); index++) {
-            for (String one : of(delChar(word, index))) {
-                list.add(wordArray[index] + one);
-            }
-        }
-        return list;
-    }
-
-    private static List<String> convertWordLenthTwo(String word) {
-        List<String> list = new ArrayList<>();
-        list.add(word);
-        list.add(reverse(word));
-        return list;
-    }
-
-    private static String delChar(String word, int index) {
-        return new StringBuilder(word).deleteCharAt(index).toString();
-    }
-
-    private static String reverse(String word) {
-        return new StringBuilder(word).reverse().toString();
+        return Arrays.asList("a");
     }
 }
--- src/test/java/AnagramsTest.java
+++ src/test/java/AnagramsTest.java
@@ -6,29 +6,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class AnagramsTest {
     @Test
-    void generate_one() {
+    void should_generate_one_char_word() {
         assertThat(Anagrams.of("a")).isEqualTo(Arrays.asList("a"));
     }
-
-    @Test
-    void generate_two() {
-        assertThat(Anagrams.of("ab")).isEqualTo(Arrays.asList("ab", "ba"));
-    }
-
-    @Test
-    void generate_three() {
-        assertThat(Anagrams.of("abc")).isEqualTo(Arrays.asList("abc", "acb", "bac", "bca", "cab", "cba"));
-    }
-
-    @Test
-    void generate_four() {
-        assertThat(Anagrams.of("biro")).isEqualTo(Arrays.asList(
-                "biro", "bior", "brio", "broi", "boir", "bori",
-                "ibro", "ibor", "irbo", "irob", "iobr", "iorb",
-                "rbio", "rboi", "ribo", "riob", "robi", "roib",
-                "obir", "obri", "oibr", "oirb", "orbi", "orib"));
-    }
-
-
-
 }
