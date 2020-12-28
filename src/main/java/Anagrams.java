f046e8b48b907d90f61e8a38dc3040a3a8e35052
huisheng.jin <huisheng.jinge@gmail.com>
2020-12-28 09:44:24 +0800

    empty message

--- src/main/java/Anagrams.java
+++ src/main/java/Anagrams.java
@@ -1,24 +1,8 @@
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.apache.commons.lang3.StringUtils.reverse;
-
 public class Anagrams {
     public static List<String> of(String word) {
-        if (word.length() == 1) {
-            return Arrays.asList(word);
-        }
-        List<String> list = new ArrayList<>();
-        for (int i = 0; i < word.length(); i++) {
-            for (String str : of(delChar(word, i))) {
-                list.add(word.charAt(i) + str);
-            }
-        }
-        return list;
-    }
-
-    private static String delChar(String word, int index) {
-        return new StringBuilder(word).deleteCharAt(index).toString();
+        return Arrays.asList("a");
     }
 }
--- src/test/java/AnagramsTest.java
+++ src/test/java/AnagramsTest.java
@@ -9,26 +9,4 @@ public class AnagramsTest {
     void should_generate_one_char_word() {
         assertThat(Anagrams.of("a")).isEqualTo(Arrays.asList("a"));
     }
-
-    @Test
-    void should_generate_two_char_word() {
-        assertThat(Anagrams.of("ab")).isEqualTo(Arrays.asList("ab", "ba"));
-    }
-
-    @Test
-    void should_generate_three_char_word() {
-        assertThat(Anagrams.of("abc")).isEqualTo(Arrays.asList("abc", "acb", "bac", "bca", "cab", "cba"));
-    }
-
-    @Test
-    void should_generate_four_char_word() {
-        assertThat(Anagrams.of("biro")).isEqualTo(Arrays.asList(
-                "biro", "bior", "brio", "broi", "boir", "bori",
-                "ibro", "ibor", "irbo", "irob", "iobr", "iorb",
-                "rbio", "rboi", "ribo", "riob", "robi", "roib",
-                "obir", "obri", "oibr", "oirb", "orbi", "orib"
-        ));
-    }
-
-
 }
