a6a68d288acfcc4b89a22ef8749abcb7559c621b
huisheng.jin <huisheng.jinge@gmail.com>
2020-12-28 09:45:43 +0800

    empty message

--- src/main/java/Anagrams.java
+++ src/main/java/Anagrams.java
@@ -1,10 +1,12 @@
 import java.util.Arrays;
 import java.util.List;
 
+import static org.apache.commons.lang3.StringUtils.reverse;
+
 public class Anagrams {
     public static List<String> of(String word) {
         if (word.length() == 2) {
-            return Arrays.asList("ab", "ba");
+            return Arrays.asList(word, reverse(word));
         }
         return Arrays.asList(word);
     }
