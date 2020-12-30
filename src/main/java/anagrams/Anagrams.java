fb833e6b1ee0dba6d906405701ab27c558b64195
huisheng.jin <huisheng.jinge@gmail.com>
2020-12-30 11:39:10 +0800

    empty message

--- src/main/java/anagrams/Anagrams.java
+++ src/main/java/anagrams/Anagrams.java
@@ -2,22 +2,23 @@ package anagrams;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 
 public class Anagrams {
     public static List<String> of(String str) {
-        if (str.length() >= 2) {
-            List<String> result = new ArrayList<>();
-            for (int index = 0; index < str.length(); index++) {
-                List<String> anagramsOfTheLeft = Anagrams.of(deleteCharAt(str, index));
-                for (String anagramOfTheLeft : anagramsOfTheLeft) {
-                    result.add(str.charAt(index) + anagramOfTheLeft);
-                }
-            }
-            return result;
+        if (str.length() == 1) {
+            return Collections.singletonList(str);
         }
-        return Arrays.asList(str);
+        List<String> result = new ArrayList<>();
+        for (int index = 0; index < str.length(); index++) {
+            List<String> anagramsOfTheLeft = Anagrams.of(deleteCharAt(str, index));
+            for (String anagramOfTheLeft : anagramsOfTheLeft) {
+                result.add(str.charAt(index) + anagramOfTheLeft);
+            }
+        }
+        return result;
     }
 
     private static String deleteCharAt(String str, int index) {
