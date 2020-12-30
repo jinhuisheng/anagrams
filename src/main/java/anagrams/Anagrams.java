fca76f4ccdb2a1c97080ff55b85121bf3ac09ac8
huisheng.jin <huisheng.jinge@gmail.com>
2020-12-30 11:30:27 +0800

    empty message

--- src/main/java/anagrams/Anagrams.java
+++ src/main/java/anagrams/Anagrams.java
@@ -1,25 +1,10 @@
 package anagrams;
 
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
 
 public class Anagrams {
-    public static List<String> generate(String str) {
-        if (str.length() == 1) {
-            return Collections.singletonList(str);
-        }
-        List<String> result = new ArrayList<>();
-        for (int index = 0; index < str.length(); index++) {
-            for (String anagram : generate(deleteChar(str, index))) {
-                String newAnagram = str.charAt(index) + anagram;
-                result.add(newAnagram);
-            }
-        }
-        return result;
-    }
-
-    private static String deleteChar(String str, int index) {
-        return new StringBuilder(str).deleteCharAt(index).toString();
+    public static List<String> of(String str) {
+        return Arrays.asList("a");
     }
 }
--- src/test/java/anagrams/AnagramsTest.java
+++ src/test/java/anagrams/AnagramsTest.java
@@ -3,33 +3,12 @@ package anagrams;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AnagramsTest {
     @Test
     void should_generate_one_char_string() {
-        assertThat(Anagrams.generate("a")).isEqualTo(Arrays.asList("a"));
+        assertThat(Anagrams.of("a")).isEqualTo(Arrays.asList("a"));
     }
-
-    @Test
-    void should_generate_two_char_string() {
-        assertThat(Anagrams.generate("ab")).isEqualTo(Arrays.asList("ab", "ba"));
-    }
-
-    @Test
-    void should_generate_three_char_string() {
-        assertThat(Anagrams.generate("abc")).isEqualTo(Arrays.asList("abc", "acb", "bac", "bca", "cab", "cba"));
-    }
-
-    @Test
-    void should_generate_four_char_string() {
-        List<String> anagrams = Anagrams.generate("biro");
-        assertThat(anagrams.size()).isEqualTo(24);
-        assertThat(anagrams.get(0)).isEqualTo("biro");
-        assertThat(anagrams.get(23)).isEqualTo("orib");
-    }
-
-
 }
