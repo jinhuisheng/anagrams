5506cd643bbe076167c0aae7149a600f877cc3b3
huisheng.jin <huisheng.jinge@gmail.com>
2020-12-30 11:36:37 +0800

    empty message

--- src/main/java/anagrams/Anagrams.java
+++ src/main/java/anagrams/Anagrams.java
@@ -1,5 +1,6 @@
 package anagrams;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,15 +9,16 @@ import static org.apache.commons.lang3.StringUtils.reverse;
 public class Anagrams {
     public static List<String> of(String str) {
         if (str.length() == 3) {
-            return Arrays.asList(
-                    str.charAt(0) + Anagrams.of(deleteCharAt(str, 0)).get(0),
-                    str.charAt(0) + Anagrams.of(deleteCharAt(str, 0)).get(1),
+            List<String> result = new ArrayList<>();
+            result.add(str.charAt(0) + Anagrams.of(deleteCharAt(str, 0)).get(0));
+            result.add(str.charAt(0) + Anagrams.of(deleteCharAt(str, 0)).get(1));
+            result.add(str.charAt(1) + Anagrams.of(deleteCharAt(str, 1)).get(0));
+            result.add(str.charAt(1) + Anagrams.of(deleteCharAt(str, 1)).get(1));
+            result.add(str.charAt(2) + Anagrams.of(deleteCharAt(str, 2)).get(0));
+            result.add(str.charAt(2) + Anagrams.of(deleteCharAt(str, 2)).get(1));
 
-                    "bac",
-                    "bca",
-                    "cab",
-                    "cba"
-            );
+
+            return result;
         }
         if (str.length() == 2) {
             return Arrays.asList(str, reverse(str));
