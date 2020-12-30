d1959ba7d64b4fb452a9cba55aceb6dcf092cb3b
huisheng.jin <huisheng.jinge@gmail.com>
2020-12-30 10:59:20 +0800

    empty message

--- src/main/java/anagrams/Anagrams.java
+++ src/main/java/anagrams/Anagrams.java
@@ -10,8 +10,8 @@ public class Anagrams {
     public static List<String> generate(String str) {
         if (str.length() == 3) {
             List<String> result = new ArrayList<>();
-            result.add("abc");
-            result.add("acb");
+            result.add("a" + generate("bc").get(0));
+            result.add("a" + generate("bc").get(1));
             result.add("bac");
             result.add("bca");
             result.add("cab");
