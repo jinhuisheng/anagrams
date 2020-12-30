12cdfb15aa61f0760c5780fd647f9ae97b129353
huisheng.jin <huisheng.jinge@gmail.com>
2020-12-30 10:55:14 +0800

    empty message

--- src/main/java/anagrams/Anagrams.java
+++ src/main/java/anagrams/Anagrams.java
@@ -3,8 +3,13 @@ package anagrams;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.apache.commons.lang3.StringUtils.reverse;
+
 public class Anagrams {
     public static List<String> generate(String str) {
+        if (str.equals("ab")) {
+            return Arrays.asList("ab", reverse("ab"));
+        }
         return Arrays.asList("a");
     }
 }
--- src/test/java/anagrams/AnagramsTest.java
+++ src/test/java/anagrams/AnagramsTest.java
@@ -11,4 +11,10 @@ public class AnagramsTest {
     void should_generate_one_char_string() {
         assertThat(Anagrams.generate("a")).isEqualTo(Arrays.asList("a"));
     }
+
+    @Test
+    void should_generate_two_char_string() {
+        assertThat(Anagrams.generate("ab")).isEqualTo(Arrays.asList("ab", "ba"));
+    }
+
 }
