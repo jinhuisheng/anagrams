17c6fffae74390afac0bc5884f68254ab7a552f9
huisheng.jin <huisheng.jinge@gmail.com>
2020-12-30 11:04:52 +0800

    empty message

--- src/main/java/anagrams/Anagrams.java
+++ src/main/java/anagrams/Anagrams.java
@@ -10,13 +10,13 @@ public class Anagrams {
     public static List<String> generate(String str) {
         if (str.length() == 3) {
             List<String> result = new ArrayList<>();
-            List<String> anagramsOfMinusOneChar = generate(deleteChar(str, 0));
-            for (String anagram : anagramsOfMinusOneChar) {
+            for (String anagram : generate(deleteChar(str, 0))) {
                 result.add(str.charAt(0) + anagram);
             }
 
-            result.add(str.charAt(1) + generate(deleteChar(str, 1)).get(0));
-            result.add(str.charAt(1) + generate(deleteChar(str, 1)).get(1));
+            for (String anagram : generate(deleteChar(str, 1))) {
+                result.add(str.charAt(1) + anagram);
+            }
             result.add(str.charAt(2) + generate(deleteChar(str, 2)).get(0));
             result.add(str.charAt(2) + generate(deleteChar(str, 2)).get(1));
             return result;
