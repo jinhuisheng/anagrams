768c94a5e80fc3ecce9ed503778a0047e48f2018
huisheng.jin <huisheng.jinge@gmail.com>
2020-12-30 10:53:41 +0800

    empty message

---
+++ src/main/java/anagrams/Anagrams.java
@@ -0,0 +1,10 @@
+package anagrams;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Anagrams {
+    public static List<String> generate(String str) {
+        return Arrays.asList("a");
+    }
+}
---
+++ src/test/java/anagrams/AnagramsTest.java
@@ -0,0 +1,14 @@
+package anagrams;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AnagramsTest {
+    @Test
+    void should_generate_one_char_string() {
+        assertThat(Anagrams.generate("a")).isEqualTo(Arrays.asList("a"));
+    }
+}
